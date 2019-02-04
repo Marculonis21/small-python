4a7a1eebc8b0c5afb7e8732e45d137152134b0e4
Sruma Marek <marsrum@V1-11.gybon>
2019-02-04 15:52:57 +0100

    git updater

---
+++ gitUpdater/gitProcess.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import os
+allPath = os.popen("ls").read().split("\n")
+
+dirs = [x for x in allPath if os.path.isdir(x)]
+
+for i in dirs:
+    #os.system("git -C {}/ status".format(i))
+    
+    if(".git" in os.popen("ls {}/ -a".format(i)).read().split("\n")):
+        print("Directory {}".format(i))
+        print(30*"-")
+        os.system("git -C {}/ status".format(i))
+        print()
+        os.system("git -C {}/ pull".format(i))
+        print()
+
+    
