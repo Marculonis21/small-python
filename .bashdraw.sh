f4cff229cd64348338b40f0352a18b655329716c
Marculonis21 <marek.sruma@gmail.com>
2020-02-24 22:58:04 +0100

    gybon meme

---
+++ .bashdraw.sh
@@ -0,0 +1,30 @@
+if [ "$1" == "ferda" ] ;
+then
+echo "
+                     \`/ho:                  
+                     ++MMN-                 
+                    -m/MMMm\`                
+                   :Nm/MMMMm-               
+                 \`sMMm+MMMMMNo\`             
+               -sNMMN//MMh:mMMNs.           
+           \`:smMMMm+\` /MMh  /yhhho-         
+        \`/hNMMNdo-    /MMh   \`+dmmso/\`\`     
+       +NMMms:\`       /MMh     dMM:+dMMmo   
+     .dMMd:           /MMMMMMMMMMM:  :dMMd. 
+    \`mMMs\`            /MMd/////mMM:   \`sMMm.
+    yMMh             \`oMMh-  \`-mMM+-\`   hMMy
+    NMM+             \`:::::  \`::::::\`   +MMN
+    NMM+ \`smmmmmmmmmm+:mNms\` .ymmmdsoo\` +MMN
+    sMMd   \`yMMMMMMMMo:MMm\` :hMMd/\`     dMMs
+    .NMMs      \`.oMMMo:MMNyNMNs-       sMMN.
+     :NMMh.       yMMo:MMMMMm\`       .hMMN- 
+      .dMMMy:    -mMM/:MMNhMMh-    :yMMMd.  
+        /dMMMNdhmMMd: :MMN./mMMmhdNMMMd/    
+          .+shddyo-   :MMN.  -oyhdhs+.      
+                      :MMN.                 
+                      :MMN.                 
+                      :Mh:                  
+                      :+                    
+";
+fi
+
