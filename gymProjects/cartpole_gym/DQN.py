90dc81e6d1fd54a2b2946494758e072304364a79
Marculonis21 <marek.sruma@gmail.com>
2020-04-17 01:00:53 +0200

    DQN MOVED SOMEWHERE

--- gymProjects/cartpole_gym/DQN.py
+++ gymProjects/cartpole_gym/DQN.py
@@ -3,17 +3,30 @@
 import gym
 import numpy as np
 import random
-import matplotlib.pyplot as plt 
+from matplotlib import pyplot as plt 
 import copy
+import time
+
 
 def sigmoid(x):
     return 1/(1+np.exp(-x))
 
-def derivative(x):
-    return (x*(1-x))
+def sigmoid_derivative(x):
+    return x*(1 - x)
 
-#def sigmoid_derivative(x):
-#    return sigmoid(x) * (1 - sigmoid(x))
+def tanh(x):
+    return np.tanh(x)
+
+def tanh_derivative(x):
+    return (1-np.square(x))
+
+def relu(x):
+    return np.maximum(0, x)
+
+def relu_derivative(x):
+    x[x<=0] = 0
+    x[x>0] = 1
+    return x
 
 
 class NN:
@@ -32,6 +45,7 @@ class Agent:
     def __init__(self, net_layers):
         self.policy_net = NN(net_layers)
         self.target_net = copy.copy(self.policy_net)
+        self.layout = net_layers
 
     def copy_net(self):
         self.target_net = copy.copy(self.policy_net)
@@ -46,10 +60,14 @@ class Agent:
         self.layers.append(inp)
 
         for i in range(len(net.layerW)):
-            l = sigmoid(np.dot(self.layers[i], net.layerW[i]))
+            if(i+1 == len(net.layerW)):
+                l = tanh(np.dot(self.layers[i], net.layerW[i]))
+            else:
+                l = tanh(np.dot(self.layers[i], net.layerW[i]))
             self.layers.append(l)
 
         end = list(self.layers[-1])
+
 
         if not full_out:
             return end.index(max(end))
@@ -57,42 +75,73 @@ class Agent:
         else:
             return end
 
-    def get_current_qValues(self, exp_batch):
+    def get_current_qValues(self, state, action):
         #exp = [state, action, reward, next_state]
         pairs = []
-        for state,a,_,_ in exp_batch:
-            pairs += [self.out_action(state, True)[a]]
+        for i in range(len(state)):
+            pairs += [self.out_action(state[i], True)[action[i]]]
 
         return pairs
 
-    def get_next_qValues(self, exp_batch):
+    def get_next_qValues(self, next_state):
         values = []
-        for _,_,_,next_state in exp_batch:
-            values += [max(self.out_action(next_state, True, "target"))]
+        for i in range(len(next_state)):
+            values += [max(self.out_action(next_state[i], True, "target"))]
 
         return values
 
     def backprop(self, loss, learning_rate, experience, batch_size):
         net = copy.copy(self.policy_net)
 
+        net_len = len(self.layout)
+        w_change =  [[] for x in range(net_len)]
+
+        w3_change = []
         w2_change = []
         w1_change = []
 
-        for state,a,_,_ in experience:
+        errors = []
+        errors.append(loss)
+        deltas = []
+
+        for state,a,_,_,_ in experience:
             out = self.out_action(state, True)
 
             I_error = loss
-            I_delta = I_error * derivative(self.layers[-1])
+            I_delta = I_error * tanh_derivative(self.layers[-1])
             II_error = np.dot(I_delta, net.layerW[-1].T)
-            II_delta = II_error * derivative(self.layers[-2])
+            II_delta = II_error * tanh_derivative(self.layers[-2])
+            III_error = np.dot(II_delta, net.layerW[-2].T)
+            III_delta = III_error * tanh_derivative(self.layers[-3])
 
-            w2_change.append(np.array(learning_rate * np.array([I_delta]).T * self.layers[-2]).T)
-            w1_change.append(np.array(learning_rate * np.array([II_delta]).T * self.layers[-3]).T)
+            #for I in range(1, net_len):
+            #    deltas.append(errors[I-1] * derivative(self.layers[-I]))
+            #    errors.append(np.dot(deltas[I-1], net.layerW[-I].T))
 
+            #print(np.transpose([self.layers.flat[-2]]))
 
-        self.policy_net.layerW[-1] += np.mean(w2_change, axis=0)
-        self.policy_net.layerW[-2] += np.mean(w1_change, axis=0)
+            
+            #print(np.atleast_2d(self.layers[-2]).T.shape)
+            #print(np.atleast_2d(self.layers[-2]).T)
+            #print(self.layers[-2][None, :].T)
+            #print(type(self.layers[-2][None, :]))
+            #print(I_delta)
+            #print(np.atleast_2d(I_delta).shape)
+            #print(np.atleast_2d(I_delta))
 
+            w3_change.append(np.dot(self.layers[-2][None, :].T, I_delta[None, :]))
+            w2_change.append(np.dot(self.layers[-3][None, :].T, II_delta[None, :]))
+            w1_change.append(np.dot(self.layers[-4][None, :].T, III_delta[None, :]))
+
+            #for i in range(1, net_len):
+            #    w_change[i].append(np.dot(self.layers[-net_len+i-1].T, deltas[-i]))
+
+        #for i in range(1, net_len):
+        #    self.policy_net.layerW[-net_len+i] += np.mean(w_change[i] * learn_rate, axis=0)
+            
+        self.policy_net.layerW[-1] += np.mean(w3_change, axis=0) * learning_rate
+        self.policy_net.layerW[-2] += np.mean(w2_change, axis=0) * learning_rate
+        self.policy_net.layerW[-3] += np.mean(w1_change, axis=0) * learning_rate
 
 class ReplayMemory:
     def __init__(self, capacity):
@@ -107,97 +156,246 @@ class ReplayMemory:
             self.memory[self.push_count % self.capacity] = exp
             self.push_count += 1
 
-    def sample_min(self, batch_size):
-        return (len(self.memory) > batch_size)
+    def sample_min(self, buffer_size):
+        return (len(self.memory) > buffer_size)
 
     def sample(self, batch_size):
         return random.sample(self.memory, batch_size)
 
-def plot(values, moving_avg_period):
-    plt.figure(2)
+    def unzip(self, batch):
+        _state = []
+        _action = []
+        _reward = []
+        _state_next = []
+        _terminal = []
+
+        for state, action, reward, state_next, terminal in batch:
+            _state.append(state)
+            _action.append(action)
+            _reward.append(reward)
+            _state_next.append(state_next)
+            _terminal.append(terminal)
+
+        return _state,_action,_reward,_state_next,_terminal
+
+def plot(values, moving_avg_period, epsilon):
+    plt.figure(1)
     plt.clf()
     plt.title('Training..')
     plt.xlabel('Episode')
     plt.ylabel('Duration')
-    plt.plot(values)
+    plt.plot(values, label='time-steps')
 
-    #moving_avg = get_moving_average(moving_avg_period, values)
-    #plt.plot(moving_avg)
-    plt.pause(0.001)
-    #print("Episode", len(values), "\n", \
-            #    moving_avg_period, "episode moving avg:", moving_avg[-1])
-    #if is_ipython: display.clear_output(wait=True)
+    x = []
+    moving_avg = []
+    moving_avg_index = []
+    loop = 0
+    for i in range(len(values)):
+        x += [values[i]]
+        if(i % moving_avg_period == 0):
+            moving_avg += [np.mean(x)]
+            moving_avg_index += [loop*moving_avg_period]
+            loop += 1
+            x = []
+
+    if(moving_avg != []):
+        plt.plot(moving_avg_index, moving_avg, label='average')
+    
+    plt.legend(loc='upper left', fontsize=9)
+
+    plt.tight_layout()
+
+    plt.pause(0.01)
+
+def sub_plot(values, moving_avg_period, epsilon):
+    plt.clf()
+    plt.title('Training..')
+
+    plt.subplot(2, 1, 1)
+    plt.xlabel('Episode')
+    plt.ylabel('Duration')
+    plt.plot(values, label='time-steps')
+
+    x = []
+    moving_avg = []
+    moving_avg_index = []
+    loop = 0
+    for i in range(len(values)):
+        x += [values[i]]
+        if(i % moving_avg_period == 0):
+            moving_avg += [np.mean(x)]
+            moving_avg_index += [loop*moving_avg_period]
+            loop += 1
+            x = []
+
+    if(moving_avg != []):
+        plt.plot(moving_avg_index, moving_avg, label='average')
+    plt.legend(loc='upper left', fontsize=9)
+    plt.tight_layout()
+    
+    plt.subplot(2, 1, 2)
+    plt.xlabel('Episode')
+    plt.ylabel('Epsilon-decay')
+    plt.plot(epsilon, label='epsilon')
+
+    plt.legend(loc='upper left', fontsize=9)
+    plt.tight_layout()
+
+    plt.pause(0.0001)
 
 if __name__ == "__main__":
-    max_steps = 500
-    batch_size = 50 
-    target_update = 10 
+    plt.style.use('ggplot')
+
+    HYPER_PARAM_TEST = True
+    HYPER_PARAM_TEST_VALUES = [{'agent':[4,32,32,2],
+                                'memory_size':100000,
+                                'max_steps':250,
+                                'batch_size':200,
+                                'buffer_size':500,
+                                'target_update':50,
+                                'learning_rate':0.001,
+                                'gamma':0.95,
+                                'epsilon':1.0,
+                                'epsilon_decay':0.999,
+                                'epsilon_end':0.02},
+                                {'agent':[4,64,32,2],
+                                'memory_size':100000,
+                                'max_steps':250,
+                                'batch_size':200,
+                                'buffer_size':500,
+                                'target_update':100,
+                                'learning_rate':0.001,
+                                'gamma':0.95,
+                                'epsilon':1.0,
+                                'epsilon_decay':0.999,
+                                'epsilon_end':0.02},
+                                {'agent':[4,64,64,2],
+                                'memory_size':100000,
+                                'max_steps':250,
+                                'batch_size':200,
+                                'buffer_size':500,
+                                'target_update':150,
+                                'learning_rate':0.001,
+                                'gamma':0.95,
+                                'epsilon':1.0,
+                                'epsilon_decay':0.999,
+                                'epsilon_end':0.02}]
+
+    episode_count = 1000
+    memory_size = 100000
+
+    max_steps = 250
+    batch_size = 128
+    buffer_size = 500 
+    target_update = 100
     learning_rate = 0.001
-    gamma = 0.999
-    #epsilon + decay
-
-    np.random.seed(0)
-
-    agent = Agent([4,5,2])
-    reMemory = ReplayMemory(100000)
+    gamma = 0.95
+    epsilon = 1.0
+    epsilon_decay = 0.999
+    epsilon_end = 0.02
 
     env = gym.make('CartPole-v0')
     env._max_episode_steps = max_steps
 
-    #print(env.action_space) # 2
-    #print(env.observation_space) # 4
+    agent = Agent([4,32,16,2])
+    reMemory = ReplayMemory(memory_size)
 
-    episode_durations = []
-    episodes = 0 
+    training_cycle = -1
     while True:
-        episodes += 1
-
-        if(episodes % target_update == 0):
-            print("Episode count: " + str(episodes))
-            #START NEW + copy net
-            agent.copy_net()
-
-        state = env.reset()
-        for time in range(1000):
-            #if(episodes % 100 == 0):
-            env.render()
-
-            action = agent.out_action(state)
-            next_state, reward, done, info = env.step(action)
-        
-            if(done):
-                if(time == max_steps-1):
-                    print("solved")
-                    reward = 10
-                else:
-                    reward = -50
-
-            #exp = [state, action, reward, next_state]
-            experience = [state, action, reward, next_state]
-            reMemory.add(experience)
-            state = next_state
-
-            if(reMemory.sample_min(batch_size)):
-                ### learn policy + maybe change target ###
-
-                exp_batch = reMemory.sample(batch_size)
-                
-                rewards = []
-                for _,_,reward,_ in exp_batch:
-                    rewards.append(reward)
-
-                current_q_values = agent.get_current_qValues(exp_batch)
-                next_q_values = agent.get_next_qValues(exp_batch)
-                target_q_values = (np.array(next_q_values) * gamma) + rewards
-
-                #LOSS = MSE => print((1/200)*sum((target_q_values - current_q_values)**2))
-                loss = np.mean((current_q_values - target_q_values)**2)
-                
-                agent.backprop(loss, learning_rate, exp_batch, batch_size)
-
-            if done:
-                episode_durations.append(time)
-                #if(episodes % 100 == 0):
-                plot(episode_durations, 100)
+        if(HYPER_PARAM_TEST):
+            training_cycle += 1
+            if(training_cycle == len(HYPER_PARAM_TEST_VALUES)):
+                print("----- END OF TRAINING CYCLE ----- ")
                 break
+
+            print("\n\n\nTraining Cycle: {}".format(training_cycle))
+            test = HYPER_PARAM_TEST_VALUES[training_cycle]
+
+            agent = Agent(test['agent'])
+            reMemory = ReplayMemory(test['memory_size'])
+            max_steps = test['max_steps']
+            batch_size = test['batch_size']
+            buffer_size = test['buffer_size']
+            target_update = test['target_update']
+            learning_rate = test['learning_rate']
+            gamma = test['gamma']
+            epsilon = test['epsilon']
+            epsilon_decay = test['epsilon_decay']
+            epsilon_end = test['epsilon_end']
+            print("Max Episodes: {}\nAgent: {}".format(episode_count, test['agent']))
+    
+        print("\nHyper-Parameters:\nMax sim steps: {}\nBatch size: {}\nBuffersize: {}\nTarget update: {}\nLearning rate: {}\nGamma value: {}\nEpsilon: {}\nEpsilon decay: {}\nEpsilon end: {}\n#####################".format(max_steps,batch_size,buffer_size,target_update,learning_rate,gamma,epsilon,epsilon_decay,epsilon_end))
+
+        env = gym.make('CartPole-v0')
+        env._max_episode_steps = max_steps
+
+        episode_durations = []
+        epsilon_mem = []
+
+        for episodes in range(episode_count):
+            loss_counter = 0
+
+            if(episodes % target_update == 0):
+                #print("Episode count: " + str(episodes))
+                agent.copy_net()
+
+            state = env.reset()
+            for time in range(1000):
+                env.render()
+
+                # EXPLORE / EXPLOIT
+                if(random.random() <= epsilon):
+                    action = env.action_space.sample()
+                else: 
+                    action = agent.out_action(state)
+
+                if(epsilon > epsilon_end):
+                    epsilon *= epsilon_decay
+
+                next_state, reward, terminal, info = env.step(action)
+            
+                reward = reward if not terminal else -reward
+
+                #exp = [state, action, reward, next_state, terminal]
+                experience = [state, action, reward, next_state, terminal]
+                reMemory.add(experience)
+                state = next_state
+
+                if(terminal):
+                    episode_durations.append(time)
+                    epsilon_mem.append(epsilon)
+                    #plot(episode_durations, 10)
+                    sub_plot(episode_durations, target_update, epsilon_mem)
+
+                    break
+
+                if(reMemory.sample_min(buffer_size)):
+                    batch = reMemory.sample(batch_size)
+                    m_state, m_action, m_reward, m_next_state, m_terminal = reMemory.unzip(batch)
+                    
+                    current_q_values = agent.get_current_qValues(m_state,m_action)
+                    next_q_values = agent.get_next_qValues(m_next_state)
+                    target_q_values = (np.array(next_q_values) * gamma) + m_reward
+
+                    for i, done in enumerate(m_terminal):
+                        if(done):
+                            target_q_values.flat[i] = m_reward[i]
+
+                    #LOSS = MSE
+                    loss = np.mean((target_q_values - current_q_values)**2)
+                    
+                    agent.backprop(loss, learning_rate, batch, batch_size)
+
+        #sub_plot(episode_durations, target_update, epsilon_mem)
+        plt.savefig("train_data/graphs/Graph_{}_{}_{}_{}_{}_{}_{}_{}_{}_{}_{}_end.png".format(agent.layout,
+                                                                                              memory_size,
+                                                                                              max_steps,
+                                                                                              batch_size,
+                                                                                              buffer_size,
+                                                                                              target_update,
+                                                                                              learning_rate,
+                                                                                              gamma,
+                                                                                              epsilon,
+                                                                                              epsilon_decay,
+                                                                                              epsilon_end))
 
