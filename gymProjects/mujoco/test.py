6c3be45bec8116f5e71742410cc2517aa4a87d2e
Marculonis21 <markbecvar@gmail.com>
2022-04-02 17:07:59 +0200

    video

--- gymProjects/mujoco/test.py
+++ gymProjects/mujoco/test.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 import copy
+import time
 
 def random_population(population_size, action_size, action_count):
     population = []
@@ -15,7 +16,7 @@ def random_population(population_size, action_size, action_count):
 
     return population
 
-def selection(population, fitness_values, k=5): # TOURNAMENT
+def tournament_selection(population, fitness_values, k=5): # TOURNAMENT
     new_population = []
     for i in range(0,len(population)):
         individuals = []
@@ -79,12 +80,19 @@ def mutation(population,indiv_mutation_prob=0.3,action_mutation_prob=0.05):
     return new_population
 
 env = gym.make('Ant-v3', reset_noise_scale=0.0)
-env._max_episode_steps = 250
+env._max_episode_steps = 500
 
+# defaults for rewards
+# "forward_reward_weight = 1" - missing
+# ctrl_cost_weight = 0.5,
+# contact_cost_weight = 5e-4,
+# healthy_reward = 1.0,
+
+# Sim steps = 5*max_episode_steps 
 def evolution():
-    population = random_population(50, 8, 250)
+    population = random_population(50, 8, env._max_episode_steps)
 
-    for generations in range(500):
+    for generations in range(10):
         if generations % 25 == 0:
             print(generations)
 
@@ -97,7 +105,9 @@ def evolution():
             observation = env.reset()
             while not done:
                 steps += 1
-                observation, reward, done, _ = env.step(individual[steps])
+
+                # INFO contains useful reward info
+                observation, reward, done, info = env.step(individual[steps])
                 if ID == 0 and generations % 10 == 0:
                     env.render()
 
@@ -111,7 +121,7 @@ def evolution():
         best_individual = population[np.argmax(fitness_values)]
 
         # selection, crossover, mutation
-        parents = selection(population,fitness_values)
+        parents = tournament_selection(population,fitness_values)
         children = crossover_uniform(parents)
         mutated_children = mutation(children)
         population = mutated_children
@@ -119,6 +129,23 @@ def evolution():
         # elitism
         population[0] = best_individual
 
-best, max_fitness, pop = evolution()
-print(best)
-print(max_fitness)
+    return best_individual
+
+best = evolution()
+
+env = gym.wrappers.Monitor(env, 'video/test', force=True)
+
+print("LAST RUN")
+
+steps = -1
+individual_reward = 0
+done = False
+observation = env.reset()
+while not done:
+    steps += 1
+    observation, reward, done, _ = env.step(best[steps])
+    env.render()
+
+    individual_reward += reward
+
+print("Last run - Best reward: ", individual_reward)
